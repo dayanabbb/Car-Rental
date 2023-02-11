e195206c3b2e2441ef5d0e19ef77c6034f8934ea
rulik04 <220625@astanait.edu.kz>
2023-02-11 21:41:46 +0600

    main+functions

--- src/Car.java
+++ src/Car.java
@@ -6,6 +6,8 @@ public class Car {
     private String type;
     private String color;
     private boolean status;
+
+
     public Car(){
 
     }
@@ -18,46 +20,61 @@ public class Car {
         this.type = type;
         this.status = status;
     }
+
     public int getId() {
         return id;
     }
+
     public int getYear() {
         return year;
     }
+
     public String getBrand() {
         return brand;
     }
+
     public String getColor() {
         return color;
     }
+
     public String getModel() {
         return model;
     }
+
     public boolean getStatus() {
         return status;
     }
+
     public String getType() {
         return type;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public void setBrand(String brand) {
         this.brand = brand;
     }
+
     public void setColor(String color) {
         this.color = color;
     }
+
     public void setModel(String model) {
         this.model = model;
     }
+
     public void setStatus(boolean status) {
         this.status = status;
     }
+
     public void setType(String type) {
         this.type = type;
     }
+
     public void setYear(int year) {
         this.year = year;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Connection connection = null;
-        ResultSet resultSet = null;
         Statement statement = null;
-        String login = null;
+        String username;
         String password = null;
         Client client = new Client();
         try {
@@ -24,62 +23,200 @@ public class Main {
             int res = scan.nextInt();
             if (res == 2) {
                 Registration();
-                //System.out.println("Thank you for registration, " + client.getName()+" " + client.getSurname());
             } else if (res == 1) {
-                System.out.print("login:");
-                login = scan.next();
+                System.out.print("username:");
+                username = scan.next();
                 System.out.print("password:");
                 password = scan.next();
-                client = Sign_in(login, password);
+                client = Sign_in(username, password);
             }
-        }
 
-        public static void Registration() {
-            String SQL_INSERT = "insert into clients (name, surname, login, password , telephone) values (?, ?, ?, ?, ?)";
-            try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-                 PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT)) {
-                String name, surname, login, password, telephone;
-                System.out.println("First name: ");
-                name = scan.nextLine();
-                preparedStatement.setString(1, name);
-                System.out.println("Second name: ");
-                surname = scan.nextLine();
-                preparedStatement.setString(2, surname);
-                System.out.println("Login: ");//check for uniqueness
-                login = scan.nextLine();
-                preparedStatement.setString(3, login);
-                System.out.println("Password: ");//check for 1-8, upper case, restricted symbols
-                password = scan.nextLine();
-                preparedStatement.setString(4, password);
-                System.out.println("Telephone: ");
-                telephone = scan.nextLine();
-                preparedStatement.setString(5, telephone);
-                preparedStatement.executeUpdate();
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        }
+            System.out.println("Hello!" + " " + client.getName() + " " + client.getSurname());
+            while (true) {
+                System.out.println("Choose an option");
+                System.out.println(" 1.Rent a car \n 2.All cars \n 3.Available cars\n 4.Profile\n 5.Close");
+                int command = scan.nextInt();
+                if (command == 1) {
+                    boolean flag = true;
+                    while (flag) {
 
-        public static Client Sign_in(String login, String password) {
-            Client client = new Client();
-            String DB = "select * from clients where login = ?";
-            try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-                 PreparedStatement preparedStatement = connection.prepareStatement(DB)) {
-                preparedStatement.setString(1, login);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                    while (!resultSet.getString("password").equals(password) || !resultSet.getString("login").equals(login)) {
-                        System.out.println("try again");
-                        password = scan.next();
+                        Car car;
+                        System.out.println("Here you can rent a car that you want");
+                        System.out.println("Choose a car");
+                        showAllAvailableCars();
+                        int ID = scan.nextInt();
+                        car = rentCar(ID);
+                        System.out.println(" Choose a period of rental -15.02.23-");
+                        String time = scan.next();
+                        System.out.println(client.getName() + " rents " + car.getBrand() + " " + car.getModel() + " until " + time);
+                        System.out.println("write 5 to exit ");
+                        int num = scan.nextInt();
+                        if (num == 5) {
+                            flag = false;
+                        }
                     }
-                    client = new Client(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("surname"), resultSet.getString("login"), resultSet.getString("password"), resultSet.getString("telephone"));
+                } else if (command == 2) {
+                    boolean flag = true;
+                    while (flag) {
+                        showAllCars();
+                        System.out.println("write 5 to exit ");
+                        int num = scan.nextInt();
+                        if (num == 5) {
+                            flag = false;
+                        }
+                    }
+                } else if (command == 3) {
+                    boolean flag = true;
+                    while (flag) {
+                        showAllAvailableCars();
+                        System.out.println("write 5 to exit ");
+                        int num = scan.nextInt();
+                        if (num == 5) {
+                            flag = false;
+                        }
+                    }
 
+                } else if (command == 4) {
+                    boolean flag = true;
+                    while (flag) {
+                        checkProfile(password);
+                        System.out.println("write 5 to exit ");
+                        int num = scan.nextInt();
+                        if (num == 5) {
+                            flag = false;
+                        }
+                    }
+
+                } else if (command == 5) {
+                    System.exit(0);
                 }
-            } catch (Exception e) {
-                System.out.println(e);
             }
+        }catch (Exception e) {
+            System.out.println("Exception occurred!");
+        } finally {
+            try {
+                connection.close();
+                statement.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
 
-            return client;
+
+
+
+    }
+
+
+
+    public static void Registration() {
+        String SQL_INSERT = "insert into clients (name, surname, username, password , telephone) values (?, ?, ?, ?, ?)";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT)) {
+            String name, surname, username, password, telephone;
+            System.out.println("First name: ");
+            name = scan.nextLine();
+            preparedStatement.setString(1, name);
+            System.out.println("Second name: ");
+            surname = scan.nextLine();
+            preparedStatement.setString(2, surname);
+            System.out.println("Username: ");
+            username = scan.nextLine();
+            preparedStatement.setString(3, username);
+            System.out.println("Password: ");
+            password = scan.nextLine();
+            preparedStatement.setString(4, password);
+            System.out.println("Telephone: ");
+            telephone = scan.nextLine();
+            preparedStatement.setString(5, telephone);
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
+
+    public static Client Sign_in(String username, String password) {
+        Client client = new Client();
+        String DB = "select * from clients where username = ?";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(DB)) {
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                while (!resultSet.getString("password").equals(password) || !resultSet.getString("username").equals(username)) {
+                    System.out.println("try again");
+                    password = scan.next();
+                }
+                client = new Client(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("surname"), resultSet.getString("username"), resultSet.getString("password"), resultSet.getString("telephone"));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return client;
+    }
+
+    public static void showAllCars() {
+        String AllCars = "select * from cars";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(AllCars)) {
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " " + resultSet.getInt("year") + " " + resultSet.getString("brand") + " "
+                        + resultSet.getString("model") + " " + resultSet.getString("type") + " " + resultSet.getString("color"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void showAllAvailableCars() {
+
+        String availableCars = "SELECT * FROM cars WHERE status = '1'";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             Statement statement = connection.createStatement();) {
+            ResultSet resultSet = statement.executeQuery(availableCars);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " " + resultSet.getInt("year") + " " + resultSet.getString("brand") + " "
+                        + resultSet.getString("model") + " " + resultSet.getString("type") + " " + resultSet.getString("color"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static Car rentCar(int id) {
+        Car car = new Car();
+        String Cars = "select * from cars where id = ?";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(Cars)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                car = new Car(resultSet.getInt("id"), resultSet.getInt("year"), resultSet.getString("brand"), resultSet.getString("model"),
+                        resultSet.getString("type"), resultSet.getString("color"), resultSet.getBoolean("status"));
+            }
+        } catch (Exception e) {
+            System.out.println(e + "Connection Error!");
+        }
+
+        return car;
+    }
+
+    public static void checkProfile(String password) {
+        String users = "select * from clients where password = ? ";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(users)) {
+            preparedStatement.setString(1, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println("User: " + resultSet.getString("name") + " " + resultSet.getString("surname") + "\nUsername: " + resultSet.getString("username") + "\nPassword: "
+                        + resultSet.getString("password") + "\nTelephone: " + resultSet.getString("telephone"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
