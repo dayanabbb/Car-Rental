9e11b9f6eafd2e9906b3dffe0f2bbbba1241a37d
rulik04 <220625@astanait.edu.kz>
2023-02-22 18:55:24 +0600

    ROD

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import com.company.controllers.CarController;
+import com.company.controllers.ClientController;
+import com.company.data.PostgresDB;
+import com.company.data.interfaces.IDB;
+import com.company.repositories.CarRepository;
+import com.company.repositories.ClientRepository;
+import com.company.repositories.interfaces.ICarRepository;
+import com.company.repositories.interfaces.IClientRepository;
+
 public class Main {
     public static void main(String[] args) {
-
+        IDB DB = new PostgresDB();
+        IClientRepository repositoryClient = new ClientRepository(DB);
+        ICarRepository repositoryCar = new CarRepository(DB);
+        ClientController clientController = new ClientController(repositoryClient);
+        CarController carController = new CarController(repositoryCar);
+        MyApplication myApplication = new MyApplication(clientController, carController);
+        myApplication.application();
     }
 
 }
