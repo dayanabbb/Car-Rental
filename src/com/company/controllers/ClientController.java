f3e2447a1385d644e201937f47914e1af7e520e1
dayanabbb <124685989+dayanabbb@users.noreply.github.com>
2023-02-24 21:57:21 +0600

    Update ClientController.java

--- src/com/company/controllers/ClientController.java
+++ src/com/company/controllers/ClientController.java
@@ -27,9 +27,7 @@ public class ClientController {
         return repository.getAllClients();
     }
     
-    public Client changePass(Client client, String newPass){
-        return  repository.changePass(client, newPass);
-    }
+    
 
 
 
